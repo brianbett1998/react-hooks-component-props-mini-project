3cf7c382192f73be12418a59dad76b2196a77c66
BRIAN BETT <+>
2023-04-20 19:30:43 +0300

    Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import blogData from "../data/blog";
+import Header from "./Header";
+import About from "./about";
+import ArticleList from "./ArticleList";
 
 console.log(blogData);
 
 function App() {
   return (
     <div className="App">
+      <Header name = "My Blog"/>
+      <About image ="https://example.com/logo.png" about="Welcome to my blog!" />
+      <ArticleList posts={posts}/>
       You're on your own from here! Follow the deliverables; test things out in
       the browser as you write your code; and good luck!
     </div>
